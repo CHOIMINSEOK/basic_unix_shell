0edd84519660dbae686189ce79afde481102d58b
CHOIMINSEOK <cms3718@gmail.com>
2018-03-21 03:24:45 +0900

    Implementation of Handling User Cammand with Parameters
    TODO :
    - Not yet support to simutaneous cammand
    - There could be `Memory leak` by dynamic memory allocation

--- src/shell.c
+++ src/shell.c
@@ -1,16 +1,20 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h> 
 
 #define FORK_ERROR -1
 #define ON_CHILD 0
 const char* SHELL_NAME = "prompt>";
+const char* TOKEN = " ";
 
+char** getInputParamArray(char* inputString);
+char* getCopyString(char* string);
 
 int main(int argc, char **argv) {
 	
 	char command[50];
-	char *arg[] = {0,};
+	char** arg = NULL;
 	int pid;
 
 	while(1) {
@@ -25,12 +29,38 @@ int main(int argc, char **argv) {
 				exit(0);
 			case ON_CHILD : 
 				printf("on child process : %d, command : %s\n", getpid(), command);
-				arg[0] = &command;
-				arg[1] = NULL; 
-				execvp(command, arg);	
+				arg = getInputParamArray(command);
+				execvp(arg[0], arg);	
 				break;
 			default :
 				printf("on parent process : %d with child : %d\n", getpid(), pid);
+				wait(0);
 		}
 	}
+}
+
+char** getInputParamArray(char* inputString) {
+	int ARRAY_SIZE_UNIT = 10;
+	char ** inputParams = (char**) malloc(sizeof(char*)*ARRAY_SIZE_UNIT);
+	char* ptr = strtok(inputString, TOKEN);
+ 	int i = 0, j = 0;
+
+	inputParams[i++] = getCopyString(ptr);
+
+	while(ptr=strtok(NULL, TOKEN)) {
+		if (ARRAY_SIZE_UNIT <= i) {
+			ARRAY_SIZE_UNIT += ARRAY_SIZE_UNIT;
+			realloc(inputParams, ARRAY_SIZE_UNIT);
+		}
+		
+		inputParams[i++] = getCopyString(ptr);
+	}
+
+	return inputParams;
+}
+
+char* getCopyString(char* string) {
+	char* ptr = (char*) malloc(sizeof(char)*(strlen(string)+1));
+	strcpy(ptr, string);
+	return ptr;
 }
