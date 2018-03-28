98edcfb94455ae94ecb960fc955ad30a62194fab
CHOIMINSEOK <cms3718@gmail.com>
2018-03-29 00:57:07 +0900

    Implement Shell Interface for `Interactive mode` & `Batch mode`

--- src/shell.c
+++ src/shell.c
@@ -2,52 +2,76 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h> 
+#include <fcntl.h>
 
 #define FORK_ERROR -1
 #define ON_CHILD 0
+
 const char* SHELL_NAME = "prompt>";
-const char* TOKEN = " ";
+const char* PARAM_TOKEN = " ";
+const char* CMD_TOKEN = ";\n";
+const int BUFFER_SIZE = 1024;
 
 char** getInputParamArray(char* inputString);
 char* getCopyString(char* string);
 
 int main(int argc, char **argv) {
 	
-	char command[50];
+	char input[BUFFER_SIZE];
+	char* command = NULL;
 	char** arg = NULL;
-	int pid;
+	int pid, fd;
+	
+
+	if(argc==2) {
+		if((fd=open(argv[1], O_RDONLY))==-1) return 0;
+		else {
+			read( fd, input, BUFFER_SIZE);
+			close(fd);
+		}
+	}
 
 	while(1) {
-		fputs(SHELL_NAME, stdout);
-		gets(command);
+		int	childNum = 0;
 
-		pid = fork();
-	
-		switch(pid){
-			case FORK_ERROR : 
-				perror("fork error: ");
-				exit(0);
-			case ON_CHILD : 
-				printf("on child process : %d, command : %s\n", getpid(), command);
-				arg = getInputParamArray(command);
-				execvp(arg[0], arg);	
-				break;
-			default :
-				printf("on parent process : %d with child : %d\n", getpid(), pid);
-				wait(0);
+		if(argc==1){
+			fputs(SHELL_NAME, stdout);
+			if(gets(input)==NULL) return 0;
 		}
+
+		command = strtok(input, CMD_TOKEN);
+		do {
+			pid = fork();
+			childNum++;
+
+			switch(pid){
+				case FORK_ERROR : 
+					perror("fork error: ");
+					exit(0);
+				case ON_CHILD : 
+					arg = getInputParamArray(command);
+					execvp(arg[0], arg);
+			} 	
+		} while(command=strtok(NULL, CMD_TOKEN));
+
+
+		while(childNum--) {
+			wait(0);
+		}
+
+		if(argc==2) return 0;
 	}
 }
 
 char** getInputParamArray(char* inputString) {
 	int ARRAY_SIZE_UNIT = 10;
 	char ** inputParams = (char**) malloc(sizeof(char*)*ARRAY_SIZE_UNIT);
-	char* ptr = strtok(inputString, TOKEN);
+	char* ptr = strtok(inputString, PARAM_TOKEN);
  	int i = 0, j = 0;
 
 	inputParams[i++] = getCopyString(ptr);
 
-	while(ptr=strtok(NULL, TOKEN)) {
+	while(ptr=strtok(NULL, PARAM_TOKEN)) {
 		if (ARRAY_SIZE_UNIT <= i) {
 			ARRAY_SIZE_UNIT += ARRAY_SIZE_UNIT;
 			realloc(inputParams, ARRAY_SIZE_UNIT);
